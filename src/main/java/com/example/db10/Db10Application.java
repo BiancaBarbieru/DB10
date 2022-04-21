b94066d843a94c2f0ed4fe6c7f6488bbc4da614a
BiancaBarbieru <biancabarbieru401@gmail.com>
2022-04-21 20:35:00 +0300

    db

---
+++ src/main/java/com/example/db10/Db10Application.java
@@ -0,0 +1,13 @@
+package com.example.db10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Db10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/db10/Db10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.db10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Db10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
